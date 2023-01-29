ae54a752cc1e4f74b3f43b84eae6c69bd7b1e0da
Maria Aguirre <mercy27.ma@gmail.com>
2023-01-29 12:58:14 -0800

    index.html script.js style.css

---
+++ LikeButton/script.js
@@ -0,0 +1,38 @@
+var count=3;
+var countElement=document.querySelector("#count");
+console.log(countElement);
+    
+function add3(){
+    count++;
+    countElement.innerText="like(s)"+count;
+    console.log(count)
+}
+
+var more=9;
+var moreElement=document.querySelector("#more");
+console.log(moreElement);
+    
+function add9(){
+    more++;
+    moreElement.innerText="like(s)"+more;
+    console.log(more)
+}
+
+var big=12;
+var bigElement=document.querySelector("#big");
+console.log(bigElement);
+
+function add12(){
+    big++;
+    bigElement.innerText="like(s)"+big;
+    console.log(big)
+}
+var jim=9;
+var jimElement=document.querySelector("#jim");
+console.log(jimElement);
+    
+function add10(){
+    jim++;
+    jimElement.innerText="like(s)"+jim;
+    console.log(jim)
+}
